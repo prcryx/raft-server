7531009833531beea9ee3f994abac2a16f156297
prcryx <me.prc.fo@gmail.com>
2023-10-27 21:48:56 +0530

    add and improve auth related types

--- internal/domain/types/auth.types.go
+++ internal/domain/types/auth.types.go
@@ -1,17 +1,38 @@
 package types
 
-type OtpRequest struct {
-	CountryCode        string      `json:"countryCode"`
-	PhoneNo            string      `json:"phoneNo"`
+type Status string
+
+const (
+	Pending  Status = "pending"
+	Approved Status = "approved"
+	Canceled Status = "canceled"
+)
+
+type OtpReqBody struct {
+	CountryCode string `json:"countryCode"`
+	PhoneNo     string `json:"phoneNo"`
+}
+
+type OtpResBody struct {
+	PhoneNo            string `json:"phoneNo"`
+	VerificationStatus Status `json:"verificationStatus"`
+}
+
+type OtpVerificationReqBody struct {
+	PhoneNo string `json:"phoneNo"`
+	Otp     string `json:"otp"`
+}
+type OtpVerificationResBody struct {
+	PhoneNo            string `json:"phoneNo"`
+	CreatedAt          string `json:"date_created"`
+	VerificationStatus Status `json:"status"`
 }
 
 type AuthenticationResponse struct {
-	Token string `json:"token"`
-}
-
-type OtpVerificationRequestBody struct {
-	Token string
-	Otp   string
+	UID         uint   `json:"uid"`
+	AccessToken string `json:"accessToken"`
+	PhoneNo     string `json:"phoneNo"`
+	CreatedAt   int64  `json:"createdAt"`
 }
 
 type TokenRefreshRequest struct {
