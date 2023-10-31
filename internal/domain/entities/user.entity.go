65944b7f41034d9bbdbc54e3745f96e1b99f5523
prcryx <me.prc.fo@gmail.com>
2023-11-01 05:27:50 +0530

    updated user entity, seperation of data

--- internal/domain/entities/user.entity.go
+++ internal/domain/entities/user.entity.go
@@ -1,7 +1,19 @@
 package entities
 
 type UserEntity struct {
-	UID       uint   `json:"uid" gorm:"primaryKey; autoIncrement"`
-	PhoneNo   string `json:"phoneNo"`
-	CreatedAt int64  `json:"createdAt" gorm:"autoUpdateTime:nano"`
+	UID       uint   `json:"uid" gorm:"primaryKey; autoIncrement; column: uid"`
+	PhoneNo   string `json:"phoneNo" gorm:"column:phoneNo"`
+	CreatedAt int64  `json:"createdAt" gorm:"autoUpdateTime:nano; column:createdAt"`
+}
+
+type User struct {
+	*UserEntity
+	AccessToken string `json:"accessToken"`
+}
+
+func (userEntity *UserEntity) ToUser(accessToken string) *User {
+	return &User{
+		UserEntity:  userEntity,
+		AccessToken: accessToken,
+	}
 }
